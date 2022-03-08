5d47ffe754ac24d302c1de7e83ddf4ec14435f5e
alexis691 <aguilar.you17@gmail.com>
2022-03-08 10:05:27 -0600

    :tada: strapi deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7ef5b64341068583a604d931d3b54985'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '779a0bc1-8b6f-4e34-a601-a59ca78ce5a3'
+};
